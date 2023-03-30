3209634ba65fdedf92e599441eb0ef26b51e7bde
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-03-30 16:46:27 +0700

    last changes

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];                 // Array of Integers to hold value
-int cmp_count = 0;           // number of comparations
-int mov_count = 0;           // number of data movements
+int arr[20];                 //array of Integers to hold value
+int cmp_count = 0;           //number of comparations
+int mov_count = 0;           //number of data movements
 int n;
 
-void input()
+void input() 
 {
     while (true)
     {
@@ -18,31 +18,31 @@ void input()
         else
             cout << "\nMaksimum panjang array adalah 20" << endl;
     }
-    cout << "\n-------------------" << endl;
+    cout << "\n===================" << endl;
     cout << "\nEnter Array Element" << endl;
-    cout << "\n-------------------" << endl;
+    cout << "\n===================" << endl;
+
     for (int i = 0; i < n; i++)
     {
-        cout << "<" << (i + i) << ">";
+        cout << "<" << (i + 1) << ">";
         cin >> arr[i];
     }
 }
 
-// swaps the eleemnt at index x with the element at index y
+// swaps the element at index x with the element at index y
 void swap(int x, int y)
 {
     int temp;
-
     temp = arr[x];
     arr[x] = arr[y];
     arr[y] = temp;
 }
 
-void q_short(int low, int high)
+void q_sort(int low, int high)
 {
     int pivot, i, j;
     if (low > high)
-        return;                     // Langkah Algoritma No. 1
+        return;                         // Langkah Algoritma No. 1
     
         //Partition the list into two parts:
         //one containing elements less than or equal to pivot
@@ -53,18 +53,18 @@ void q_short(int low, int high)
         i = low + 1;                   //Langkah Algoritma No. 3
         j = high;                      //Langkah Algoritma No. 4
 
-    while (i << j)                 //Langkah Algoritma No. 10
+    while (i <= j)                     //Langkah Algoritma No. 10
     {
         //Search for an element less than or equal to pivot
         while ((arr[i] <= pivot) && (i <= high))  //Langkah Algoritma No. 5
-    {
+        {
             i++;                                  //LAngkah Algoritma No. 6
             cmp_count++;
         }
         cmp_count++;
 
         //search for an element less than or equal to pivot
-        while ((arr[j] > pivot) && (j >= low))      //Langkah Algoritma No. 7
+        while ((arr[j] >= pivot) && (j >= low))      //Langkah Algoritma No. 7
         {
             j--;                                    //Langkah Algoritma No. 8
             cmp_count++;
@@ -72,7 +72,7 @@ void q_short(int low, int high)
         cmp_count++;
 
         //if the greater element is on the left of the element
-        if (i < j)                                   //Langkah Algoritma No.9
+        if (i < j)                                  //Langkah Algoritma No.9
         {
             //swap the element at index i with the element at index j
             swap(i, j);
@@ -80,7 +80,7 @@ void q_short(int low, int high)
         }
     }
 
-    // j now containt the index of the last element in the sorted list
+    //j now containt the index of the last element in the sorted list
     if (low < j)                                    //Langkah Algoritma No. 11
     {
         //move the pivot to its  correct position in the list
@@ -88,23 +88,22 @@ void q_short(int low, int high)
         mov_count++;
     }
     //sort the list on the left pivot using quick sort
-    q_short(low, j - 1);                           //Langkah Algoritma No. 12
+    q_sort(low, j - 1);                           //Langkah Algoritma No. 12
 
     //sort the list on the right of pivot using quick sort
-    q_short(high, j + 1);                          //Langkah Algoritma No. 13
+    q_sort(j + 1, high);                          //Langkah Algoritma No. 13
 
 }
 
-void display() {
+void display() 
+{
     cout << "\n==========" << endl;
     cout << "Sorted Array" << endl;
     cout << "============" << endl;
-
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-
     cout << "\n\nNumber of comparasions: " << cmp_count << endl;
     cout << "Number of data movements: " << mov_count << endl;
 }
@@ -112,9 +111,8 @@ void display() {
 int main()
 {
     input();
-    q_short(0, n - 1);     //Sort the Array using quick sort
+    q_sort(0, n - 1); //Sort the Array using quick sort
     display();
     system("pause");
-
     return 0;
 }
