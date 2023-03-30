64114e9c187ac8cbd5d598d775bcd91d3c97e9b4
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-03-30 16:14:05 +0700

    last

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
 int arr[20];                 // Array of Integers to hold value
-int cmp_count = 0;          // number of comparations
-int mov_count = 0;     // number of data movements
+int cmp_count = 0;           // number of comparations
+int mov_count = 0;           // number of data movements
 int n;
 
 void input()
@@ -45,19 +44,19 @@ void q_short(int low, int high)
     if (low > high)
         return;                     // Langkah Algoritma No. 1
     
-    //Partition the list into two parts
-    //one containing elements less than or equal to pivot
-    //Outher containing elements greather than pivot
+        //Partition the list into two parts:
+        //one containing elements less than or equal to pivot
+        //Outher containing elements greater than pivot
 
-    pivot = arr[low];              //Langkah Algoritma No. 2
+        pivot = arr[low];              //Langkah Algoritma No. 2
 
-    i = low + 1;                   //Langkah Algoritma No. 3
-    j = high;                      //Langkah Algoritma No. 4
+        i = low + 1;                   //Langkah Algoritma No. 3
+        j = high;                      //Langkah Algoritma No. 4
 
     while (i << j)                 //Langkah Algoritma No. 10
     {
         //Search for an element less than or equal to pivot
-        while ((arr[i] <= pivot) && (j <= high))  //Langkah Algoritma No. 5
+        while ((arr[i] <= pivot) && (i <= high))  //Langkah Algoritma No. 5
     {
             i++;                                  //LAngkah Algoritma No. 6
             cmp_count++;
@@ -80,7 +79,8 @@ void q_short(int low, int high)
             mov_count++;
         }
     }
-    //j now containt the index of the last element in the sorted list
+
+    // j now containt the index of the last element in the sorted list
     if (low < j)                                    //Langkah Algoritma No. 11
     {
         //move the pivot to its  correct position in the list
@@ -88,24 +88,24 @@ void q_short(int low, int high)
         mov_count++;
     }
     //sort the list on the left pivot using quick sort
-    q_short(low, j - 1);                           // Langkah Algoritma No. 12
+    q_short(low, j - 1);                           //Langkah Algoritma No. 12
 
     //sort the list on the right of pivot using quick sort
-    q_short(j + 1, high);                          // Langkah Algoritma No. 13
+    q_short(high, j + 1);                          //Langkah Algoritma No. 13
 
 }
 
 void display() {
-    cout << "\-----------" << endl;
+    cout << "\n==========" << endl;
     cout << "Sorted Array" << endl;
-    cout << "------------" << endl;
+    cout << "============" << endl;
 
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
 
-    cout << "\n\nNumber of comparasion: " << cmp_count << endl;
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
     cout << "Number of data movements: " << mov_count << endl;
 }
 
