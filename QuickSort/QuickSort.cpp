7e2fb7215acd154ba64e35c8b5ca5d1f3395a90b
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-03-30 15:52:02 +0700

    fungsi display

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -92,7 +92,25 @@ void q_short(int low, int high)
 
     //sort the list on the right of pivot using quick sort
     q_short(j + 1, high);                          // Langkah Algoritma No. 13
+
 }
+
+void display() {
+    cout << "\-----------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasion: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
+
+
+
 
 
 
